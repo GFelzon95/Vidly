c35b3445cfe0e59a3714f16a5da17fabdfd60a6c
Gian Felzon Pascual <pascual.gianfelzon@wesleyan.edu.ph>
2023-09-30 21:16:07 +0800

    Added Customer Form

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -28,6 +28,62 @@ namespace Vidly.Controllers
         //        new Customer {Id = 1, Name = "John Smith"},
         //        new Customer {Id = 2, Name = "Mary Williams"}
         //    };
+
+        public ActionResult New()
+        {
+            var membershipTypes = _context.MembershipTypes.ToList();
+            //var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            var viewModel = new CustomerFormViewModel()
+            {
+                //Customer = customer,
+                Membershiptypes = membershipTypes
+            };
+
+            return View("CustomerForm",viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            if(customer.Id == 0)
+            {
+                _context.Customers.Add(customer);
+            }
+
+            else
+            {
+                var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == customer.Id);
+
+                customerInDb.Name = customer.Name;
+                customerInDb.BirthDate = customer.BirthDate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if(customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            var viewModel = new CustomerFormViewModel()
+            {
+                Customer = customer,
+                Membershiptypes = _context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm", viewModel);
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
