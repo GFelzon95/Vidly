85b1c9ec81c5c011256c1238816c2364fccc8d17
Gian Felzon Pascual <pascual.gianfelzon@wesleyan.edu.ph>
2023-12-02 23:06:39 +0800

    Created different views for the movies list for guest and admin

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -23,6 +23,7 @@ namespace Vidly.Controllers
             _context.Dispose();
         }
 
+        [Authorize(Roles = RoleName.CanManageMovies)]
         public ActionResult New()
         {
             var viewModel = new MovieFormViewModel() 
@@ -108,14 +109,10 @@ namespace Vidly.Controllers
 
         public ActionResult Index()
         {
-            //var movies = _context.Movies.Include(m => m.Genre).ToList();
-
-            //var viewModel = new MoviesIndexViewModel()
-            //{
-            //    Movies = movies
-            //};
-
-            return View(/*viewModel*/);
+            if (User.IsInRole(RoleName.CanManageMovies))
+                return View("List");
+            
+            return View("ReadOnlyList");
         }
 
         public ActionResult Details(int id)
